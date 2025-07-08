62f011837cb3ee3773ecedfcb375dfcc05731737
Piers Anderson <piers@craft.co>
2025-07-08 10:18:26 +0100

    Completed matching of owners to acuris search results in fetch acuris individuals

--- src/processors/fetch_acuris_individuals.py
+++ src/processors/fetch_acuris_individuals.py
@@ -3,88 +3,102 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from typing import Any
 import requests
-from requests import Response
 from fetch_ubo import BeneficialOwners, get_beneficial_owners, get_individual_owners
 
 
 # Load environment variables
 load_dotenv()
-acuris_key  = os.getenv("KEY_ACURIS_TEST")
-acuris_url  = os.getenv('URL_ACURIS_INDIVIDUAL')
 
-class AcurisSearchPayload(BaseModel):
-    name: str = 'Unknown'
-    threshold: int = 95
-    countries: list[str] = ['IL','US','GB']
-    datasets: list[str] = ['PEP-LINKED','POI','SAN-FORMER','SAN-CURRENT','RRE','REL']
+acuris_key: str = os.getenv("KEY_ACURIS_TEST")
+acuris_url: str = os.getenv('URL_ACURIS_INDIVIDUAL')
 
-class AcurisMatchAddress(BaseModel):
+
+class Datasets(BaseModel):
+    datasets: list[str]
+
+class DatesOfBirth(BaseModel):
+    datesOfBirth: list[str | None]
+
+class Address(BaseModel):
     geography: str | None
     city: str | None
 
-class AcurisIndividualMatch(BaseModel):
+class Match(BaseModel):
     qrCode: str
     resourceId: str
     score: int
     match: str
     name: str
-    addresses: list[AcurisMatchAddress]
+    countries: list[str]
+    addresses: list[Address]
     datesOfBirth: list[str | None]
-    gender: str | None
-    profileImage: str | None
+    gender: str = 'Unknown'
+    profileImage: str = 'Not Available'
     datasets: list[str]
+    resourceUri: str
+    version: int
+    currentSanBodyIds: list[int]
+    formerSanBodyIds: list[int]
 
-class UboAcurisResult(BaseModel):
-    owner_name: str | None = None
-    matches: list[AcurisIndividualMatch] | None
+class Results(BaseModel):
+    matchCount: int
+    matches: list[Match]
 
-class UboAcurisResults(BaseModel):
-    match_results: list[UboAcurisResult] | None
+class ApiResponse(BaseModel):
+    results: Results
 
-    def add_results(self,uboAcurisResult: UboAcurisResult):
+class Payload(BaseModel):
+    name: str | None
+    threshold: int = 95
+    countries: list[str] = ['IL','US','GB']
+    datasets: list[str] = ['PEP-CURRENT','PEP-FORMER','PEP-LINKED','SAN-CURRENT','SAN-FORMER','RRE','POI','REL']
+
+class OwnerMatch(BaseModel):
+    owner_name: str | None
+    owner_matches: list[Match] | None
+
+class MatchedOwners(BaseModel):
+    ownerMatches: list[OwnerMatch]
+
+    def add_owner_matches(self, ownerMatches: OwnerMatch) -> None:
         """
-        Method to add a single search response from Acuris
+        Add matches for a beneficial owner
         """
-        self.match_results.append(uboAcurisResult)
-
-def search_acuris(payload) -> UboAcurisResult:
-    response: Response = requests.post(
-        url=acuris_url,
-        headers={"X-Api-Key": acuris_key},
-        json=payload
-    )
-    response_body: dict[str, Any ] = response.json()
-    matches = UboAcurisResult(
-        matches=response_body["results"]["matches"]
-    )
-
-    return matches
+        self.ownerMatches.append(ownerMatches)
 
 
-def get_individual_matches(individual_owners: BeneficialOwners) -> UboAcurisResults:
-    
-    individual_matches = UboAcurisResults(match_results=[])
+# - Functions - #
 
+def post_acuris_search(payload: dict[str,Any]) -> ApiResponse:
+    response  = requests.post(url=acuris_url,headers={"X-Api-Key": acuris_key},json=payload)
+    response.raise_for_status()
+    response_data = response.json()
+    acuris_response = ApiResponse(**response_data)
+    # acurisResults = AcurisResults(**acurisResponseData.results.model_dump())
+    return acuris_response
+
+def get_acuris_matches(individual_owners: BeneficialOwners) -> MatchedOwners:
     owner_names: list[str] = [beneficialOwner.name for beneficialOwner in individual_owners.beneficialOwners]
+
+    matched_owners = MatchedOwners(ownerMatches=[])
     for owner_name in owner_names:
-        payload = AcurisSearchPayload(
-            name=owner_name
-        )
-        owner_matches = search_acuris(payload.model_dump())
-        individual_matches.add_results(owner_matches)
-        
+        payload = Payload(name=owner_name)
+        acuris_response = post_acuris_search(payload.model_dump())
+        matches = [match for match in acuris_response.results.matches if len(acuris_response.results.matches) > 0]
+        ownerMatch = OwnerMatch(owner_name=owner_name,owner_matches=matches)
+        matched_owners.add_owner_matches(ownerMatch)
 
-    return individual_matches
+    return matched_owners
 
-# Testing with Jfrog ID
+
+# Testing wit Jfrog ID
 def main():
     craft_id: int = 60903
 
     owners_raw  = get_beneficial_owners(craft_id) # Get all ubo
     individual_owners  = get_individual_owners(owners_raw) # Individuals only
-    individual_matches = get_individual_matches(individual_owners).model_dump()
-
-    print(individual_matches)
+    output = get_acuris_matches(individual_owners)
+    print(output.model_dump())
 
 if __name__ == "__main__":
     main()
